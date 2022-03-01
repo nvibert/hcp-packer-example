458089aab95e4b6e94d79d33fe3af55bc1ecccca
Nico Vibert <nico.vibert@hashicorp.com>
2022-03-01 13:53:41 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,12 +5,12 @@ provider "aws" {
 }
 
 data "hcp_packer_iteration" "ubuntu" {
-  bucket_name = "learn-packer-ubuntu"
+  bucket_name = "learn-packer-ubuntu-2022"
   channel     = "production"
 }
 
 data "hcp_packer_image" "ubuntu_us_east_2" {
-  bucket_name    = "learn-packer-ubuntu"
+  bucket_name    = "learn-packer-ubuntu-2022"
   cloud_provider = "aws"
   iteration_id   = data.hcp_packer_iteration.ubuntu.ulid
   region         = "us-east-2"
